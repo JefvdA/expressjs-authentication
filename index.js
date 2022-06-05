9ed70a979a218b064b665d9857e3ac6c2c294701
jefvda <jef.v.d.a@live.be>
2022-06-05 23:30:34 +0200

    Removed body-parser

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 
-app.use(bodyParser.json());
+app.use(express.json());
 app.use(
-  bodyParser.urlencoded({
+  express.urlencoded({
     extended: true,
   })
 );
+
 
 // DB Connection
 const mongoose = require('mongoose');
