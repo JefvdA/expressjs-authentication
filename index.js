c2d761bf391b8423be1f3d9f6b9018f4b4b8fa29
yacinec <yacinec.pro@gmail.com>
2022-06-07 12:23:09 +0200

    Remove helloworld- #14

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const cookieSession = require('cookie-session');
+const express = require("express");
+const cookieSession = require("cookie-session");
 const app = express();
 const port = 3000;
 
-const db = require('./src/models');
-const dbConfig = require('./src/config/db.config');
-const authConfig = require('./src/config/auth.config');
+const db = require("./src/models");
+const dbConfig = require("./src/config/db.config");
+const authConfig = require("./src/config/auth.config");
 
 app.use(express.json());
 app.use(
@@ -15,41 +15,37 @@ app.use(
 );
 app.use(
   cookieSession({
-    name: 'chat-app-session',
+    name: "chat-app-session",
     secret: authConfig.cookieSecret,
     httpOnly: true,
   })
 );
-app.use(function(req, res, next) {
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, Content-Type, Accept"
-  );
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Headers", "Origin, Content-Type, Accept");
   next();
 });
 
 // DB Connection
-db.mongoose.connect(dbConfig.url, dbConfig.options)
-.then(() => console.log('DB Connected'))
-.catch(err => console.log(err));
+db.mongoose
+  .connect(dbConfig.url, dbConfig.options)
+  .then(() => console.log("DB Connected"))
+  .catch((err) => console.log(err));
 
 db.init();
 
 // Test route
-app.get('/api', (req, res) => {
-  res.json({'message': 'ok'});
+app.get("/api", (req, res) => {
+  res.json({ message: "ok" });
 });
 
 // Import routes
-const helloworldRouter = require('./src/routes/helloworld.routes');
-const authRouter = require('./src/routes/auth.routes');
-const testRouter = require('./src/routes/test.routes');
+const authRouter = require("./src/routes/auth.routes");
+const testRouter = require("./src/routes/test.routes");
 
 // Use routes
-app.use('/api/helloworld', helloworldRouter);
-app.use('/api/auth', authRouter);
-app.use('/api/test', testRouter);
+app.use("/api/auth", authRouter);
+app.use("/api/test", testRouter);
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}!`);
+  console.log(`Example app listening on port ${port}!`);
 });
--- src/routes/helloworld.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const helloworldController = require('../controllers/helloworld.controller');
-
-router.get('/', helloworldController.get);
-router.post('/', helloworldController.set);
-
-module.exports = router;
